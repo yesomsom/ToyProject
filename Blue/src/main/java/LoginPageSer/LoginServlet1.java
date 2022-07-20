d29b5ddfc6f5ac56b879af23f5b44da5471af6ae
yesomsom <109362777+yesomsom@users.noreply.github.com>
2022-07-21 00:01:42 +0900

    Add files via upload

--- Blue/src/main/java/LoginPageSer/LoginServlet1.java
+++ Blue/src/main/java/LoginPageSer/LoginServlet1.java
@@ -1,47 +1,30 @@
 package LoginPageSer;
 
 import java.io.IOException;
-import javax.servlet.ServletConfig;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LoginServlet1
- */
 @WebServlet("/loginServlet1")
 public class LoginServlet1 extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet1() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
-	public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
+	public void init() throws ServletException {
+		System.out.println("LoginServlet1 init 메서드 호출");
 	}
 
-	/**
-	 * @see Servlet#destroy()
-	 */
 	public void destroy() {
-		// TODO Auto-generated method stub
+		System.out.println("LoginServlet1 destroy 메서드 호출");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		String id = request.getParameter("user_id");
+		String pw = request.getParameter("user_pw");
 	}
 
 }
