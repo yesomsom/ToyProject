935dfafe7e49a294ea265dd0d7abfe5048140880
yesomsom <109362777+yesomsom@users.noreply.github.com>
2022-11-03 23:35:15 +0900

    Delete java/mango directory

--- java/mango/common/util/AdminURLValue.java
+++
@@ -1,9 +0,0 @@
-package mango.common.util;
-
-public class AdminURLValue {
-	
-	public static final String ADMIN = "admin"; // 관리자 페이지 기본 경로
-	
-	public static final String BOARD = ADMIN + "/board"; // 게시판 기본 경로
-	
-}
