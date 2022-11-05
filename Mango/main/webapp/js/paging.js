f650926ba029ad559a6f6f7006ddd391503037e1
yesomsom <109362777+yesomsom@users.noreply.github.com>
2022-11-06 02:09:41 +0900

    Add files via upload

--- Mango/main/webapp/js/paging.js
+++ Mango/main/webapp/js/paging.js
@@ -1,36 +1,14 @@
 $(".pageInfo a").on("click", function(e) {
 	e.preventDefault();
 	$("#pageNum").val($(this).attr("href"));
+	
 	$("#qaForm").submit();
-})
-$(".pageInfo_btn a").on("click", function(e) {
-	e.preventDefault();
-	$("#pageNum").val($(this).attr("href"));
 	$("#noticeForm").submit();
-})
-$(".pageInfo a").on("click", function(e) {
-	e.preventDefault();
-	$("#pageNum").val($(this).attr("href"));
 	$("#myAskList").submit();
-})
-$(".pageInfo a").on("click", function(e) {
-	e.preventDefault();
-	$("#pageNum").val($(this).attr("href"));
 	$("#goodsForm").submit();
 	$("#goodsKeywordForm").submit();
-})
-$(".pageInfo a").on("click", function(e) {
-	e.preventDefault();
-	$("#pageNum").val($(this).attr("href"));
 	$("#goodsListForm").submit();
-})
-$(".pageInfo a").on("click", function(e) {
-	e.preventDefault();
-	$("#pageNum").val($(this).attr("href"));
 	$("#goodsManagementForm").submit();
+	$("#ordersListForm").submit();
+	$("#salesManagementForm").submit();
 })
-$(".pageInfo a").on("click", function(e) {
-		e.preventDefault();
-		$("#pageNum").val($(this).attr("href"));
-		$("#ordersListForm").submit();
-	})
