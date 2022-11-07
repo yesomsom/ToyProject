dfbfef6578a63180d1e83f51f7918f44a3dbe580
yesomsom <109362777+yesomsom@users.noreply.github.com>
2022-11-07 15:49:03 +0900

    Add files via upload

--- Mango/main/webapp/js/searchKeyword.js
+++ Mango/main/webapp/js/searchKeyword.js
@@ -1,6 +1,7 @@
 $(document).on('click', '#btnSearch', function(e){
       e.preventDefault();
       var url= "${getMemberListURL}";
+      alert(url);
       url = url + "?searchType=" + $('#searchType').val();
       url = url + "&keyword=" + $('#keyword').val();
       location.href = url;
