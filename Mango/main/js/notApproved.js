d70984da70f2a50c049b182777dcf6c398972967
yesomsom <109362777+yesomsom@users.noreply.github.com>
2022-11-09 09:23:29 +0900

    Delete Mango/main/js directory

--- Mango/main/js/notApproved.js
+++
@@ -1,4 +0,0 @@
-$("#sendBtn").on('click',function(){
-	$("#notApprovedForm").submit()
-	alert("상품이 승인되었습니다.")
-})
--- Mango/main/js/salesDetails.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	/* option selected 값으로 변경해서 전달 */
-	$("select[name=deliveryState]").change(function() {
-		let selectVal = $(this).val();
-		$("option[name=delivery]").val(selectVal);
-	});
-});
