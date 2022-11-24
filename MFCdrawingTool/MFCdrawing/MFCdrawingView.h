9d4899c860a77e8a2e4d75131041492e1f5f419e
yesomsom <109362777+yesomsom@users.noreply.github.com>
2022-11-24 10:05:31 +0900

    Add files via upload

---
+++ MFCdrawingTool/MFCdrawing/MFCdrawingView.h
@@ -0,0 +1,59 @@
+﻿
+// MFCdrawingView.h: CMFCdrawingView 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CMFCdrawingView : public CView
+{
+protected: // serialization에서만 만들어집니다.
+	CMFCdrawingView() noexcept;
+	DECLARE_DYNCREATE(CMFCdrawingView)
+
+// 특성입니다.
+public:
+	CMFCdrawingDoc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual void OnDraw(CDC* pDC);  // 이 뷰를 그리기 위해 재정의되었습니다.
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 구현입니다.
+public:
+	virtual ~CMFCdrawingView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+private:
+	CPoint m_point; // Start point
+	UINT m_nChar; // 'L' 'R' 'C'
+public:
+	afx_msg void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
+	afx_msg void OnLine();
+	afx_msg void OnRect();
+	afx_msg void OnCircle();
+};
+
+#ifndef _DEBUG  // MFCdrawingView.cpp의 디버그 버전
+inline CMFCdrawingDoc* CMFCdrawingView::GetDocument() const
+   { return reinterpret_cast<CMFCdrawingDoc*>(m_pDocument); }
+#endif
+
---
+++ MFCdrawingTool/MFCdrawing/resource.h
@@ -0,0 +1,22 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// MFCdrawing.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_MFCdrawingTYPE              130
+#define ID_LINE                         32774
+#define ID_RECT                         32775
+#define ID_CIRCLE                         32776
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
